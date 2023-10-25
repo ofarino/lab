3d80e7746738b8248d56b23d31a13161afe9b29e
Olivia Farino <ofarino@icloud.com>
2023-10-25 12:43:21 -0600

    encoder and main

---
+++ main.py
@@ -0,0 +1,31 @@
+# Olivia Farino
+def encode(password):
+    encoded_password = ""
+    for digit in password:
+        encoded_digit = str((int(digit) + 3) % 10)
+        encoded_password += encoded_digit
+
+    return encoded_password
+
+
+if __name__ == "__main__":
+    while True:
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+
+        choice = input("Please enter an option: ")
+
+        if choice == "1":
+            password = input("Please enter your password to encode: ")
+            encoded_password = encode(password)
+            print("Your password has been encoded and stored!")
+        elif choice == "2":
+            encoded_password = input("Please enter the encoded password to decode: ")
+            pass
+            # original_password = decode(encoded_password)
+            # print(f"The encoded password is {encoded_password}, and the original password is {original_password}.")
+        elif choice == "3":
+            break
